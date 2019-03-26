0bfd95cbf9e9a49442a13b755f2c37c14d234895
Lospring Liu <lospringliu@gmail.com>
2019-03-25 20:08:50 -0400

    prepare collabration: enforce format lint test

--- src/keypairs.js
+++ src/keypairs.js
@@ -2,73 +2,73 @@
 // this is only for jcc compatibility use
 
 class KeyPairs {
-	constructor(token = "swt") {
-		if (token.toLowerCase() === "swt") {
-			this._KEYPAIRS = require("swtc-keypairs")
-		} else if (token.toLowerCase() === "bwt") {
-			this._KEYPAIRS = require("bwt-keypairs")
-		} else {
-			//throw new Error(`no implementation for ${token} alliance chain yet`);
-			throw new Error(`config of ${token.toLowerCase()} is empty`)
-		}
-	}
+  constructor(token = "swt") {
+    if (token.toLowerCase() === "swt") {
+      this._KEYPAIRS = require("swtc-keypairs")
+    } else if (token.toLowerCase() === "bwt") {
+      this._KEYPAIRS = require("bwt-keypairs")
+    } else {
+      //throw new Error(`no implementation for ${token} alliance chain yet`);
+      throw new Error(`config of ${token.toLowerCase()} is empty`)
+    }
+  }
 
-	/**
-	 * generate random bytes and encode it to secret
-	 * @returns {string}
-	 */
-	generateSeed() {
-		return this._KEYPAIRS.generateSeed()
-	}
+  /**
+   * generate random bytes and encode it to secret
+   * @returns {string}
+   */
+  generateSeed() {
+    return this._KEYPAIRS.generateSeed()
+  }
 
-	/**
-	 * derive keypair from secret
-	 * @param {string} secret
-	 * @returns {{privateKey: string, publicKey: *}}
-	 */
-	deriveKeyPair(secret) {
-		return this._KEYPAIRS.deriveKeyPair(secret)
-	}
+  /**
+   * derive keypair from secret
+   * @param {string} secret
+   * @returns {{privateKey: string, publicKey: *}}
+   */
+  deriveKeyPair(secret) {
+    return this._KEYPAIRS.deriveKeyPair(secret)
+  }
 
-	/**
-	 * derive wallet address from publickey
-	 * @param {string} publicKey
-	 * @returns {string}
-	 */
-	deriveAddress(publicKey) {
-		return this._KEYPAIRS.deriveAddress(publicKey)
-	}
+  /**
+   * derive wallet address from publickey
+   * @param {string} publicKey
+   * @returns {string}
+   */
+  deriveAddress(publicKey) {
+    return this._KEYPAIRS.deriveAddress(publicKey)
+  }
 
-	/**
-	 * check is address is valid
-	 * @param address
-	 * @returns {boolean}
-	 */
-	checkAddress(address) {
-		return this._KEYPAIRS.checkAddress(address)
-	}
+  /**
+   * check is address is valid
+   * @param address
+   * @returns {boolean}
+   */
+  checkAddress(address) {
+    return this._KEYPAIRS.checkAddress(address)
+  }
 
-	/**
-	 * convert the given address to byte array
-	 * @param address
-	 * @returns byte array
-	 */
-	convertAddressToBytes(address) {
-		if (this.checkAddress(address)) {
-			return this._KEYPAIRS.addressCodec.decodeAddress(address)
-		} else {
-			throw new Error("convert address to bytes in error")
-		}
-	}
+  /**
+   * convert the given address to byte array
+   * @param address
+   * @returns byte array
+   */
+  convertAddressToBytes(address) {
+    if (this.checkAddress(address)) {
+      return this._KEYPAIRS.addressCodec.decodeAddress(address)
+    } else {
+      throw new Error("convert address to bytes in error")
+    }
+  }
 
-	/*
-	 * convert the byte array to wallet address
-	 */
-	convertBytesToAddress(bytes) {
-		if (typeof bytes !== "object")
-			throw new Error("convert bytes to address in error")
-		return this._KEYPAIRS.addressCodec.encodeAddress(bytes)
-	}
+  /*
+   * convert the byte array to wallet address
+   */
+  convertBytesToAddress(bytes) {
+    if (typeof bytes !== "object")
+      throw new Error("convert bytes to address in error")
+    return this._KEYPAIRS.addressCodec.encodeAddress(bytes)
+  }
 }
 
 module.exports = KeyPairs
